e0d4bb2ee2ab521ef5ace5eaee2209e2fdc803ba
Brian Foo <brian@youaremyjoy.org>
2014-03-31 12:35:41 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OralHistory::Application.config.secret_token = '9cb7ac09386ead0f0c09ca0ad6d34674ef0a05d207ad38ea4d557f74125f86f1a4d56339726c47f5900e7e4cea56b97519d96b6e89af5700578843d486b2cba4'
