b0fa8782578f13948774e097ddb40c34eefa74c9
Brian Foo <brian@youaremyjoy.org>
2014-04-04 15:14:37 -0400

    adding scss to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ OralHistory::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( *.js *.css )
+  config.assets.precompile += %w( *.js *.css *.scss )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
