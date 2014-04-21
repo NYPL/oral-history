9f81f5998076c2eb23efc3fee1c80ac7a02062e1
Brian Foo <brian@youaremyjoy.org>
2014-04-21 15:41:25 -0400

    updating google oauth scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], { 
     access_type: "offline", 
-    approval_prompt: ""
+    approval_prompt: "",
+    scope: "userinfo.email"
   }
 end
