9a479d7bb45769d1299301dd9f973d1903df4f1a
Brian Foo <brian@youaremyjoy.org>
2014-04-24 17:17:32 -0400

    fixed JST error on annotations page

--- app/assets/javascripts/annotations.js
+++ app/assets/javascripts/annotations.js
@@ -3,6 +3,7 @@
 //= require ./vendor/backbone-min
 //= require ./vendor/jquery-ui-autocomplete.min
 //= require backbone-app
+//= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views
