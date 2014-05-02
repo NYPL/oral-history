359205480eb2e3a092810a275854e12e38130da4
Brian Foo <brian@youaremyjoy.org>
2014-05-02 15:16:35 -0400

    updated schema version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140502141635) do
+ActiveRecord::Schema.define(:version => 20140502154241) do
 
   create_table "interviews", :force => true do |t|
     t.string   "slug",                              :null => false
