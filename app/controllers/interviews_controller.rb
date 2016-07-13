0c8be128fd1386f1da801bb734986b9f13aaaba7
Brian Foo <brian@youaremyjoy.org>
2016-07-13 14:10:37 -0400

    add interviews.json endpoint

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,19 +1,29 @@
 class InterviewsController < ApplicationController
-  
+
   # GET /interviews
   def index
     if params[:neighborhood_id]
       @neighborhood = Neighborhood.find_by_slug(params[:neighborhood_id])
+      @interviews = Interview.select("annotations, interviews.id, interviews.image, interviews.slug, storyteller_name, summary, url, location, notes, occupations, other_locations, place_of_birth, branch_id, branches.name AS branch_name")
+                             .joins("INNER JOIN branches ON interviews.branch_id = branches.id")
+                             .where("interviews.neighborhood_id = ? AND is_demo = ?", @neighborhood.id, 0)
+                             .order("storyteller_name")
     else
-      @neighborhood = Neighborhood.order("created_at").first
+      @interviews = Interview.select("slug, updated_at, created_at")
+                             .where("is_demo = ?", 0)
+                             .order("updated_at DESC")
+
+      @interviews.map! { |interview|
+        # interview = interview.as_json
+        interview[:url] = interview_url(interview[:slug], format: :json)
+        # interview.except(:image)
+        interview
+      }
     end
-    @interviews = Interview.select("annotations, interviews.id, interviews.image, interviews.slug, storyteller_name, summary, url, location, notes, occupations, other_locations, place_of_birth, branch_id, branches.name AS branch_name")
-                           .joins("INNER JOIN branches ON interviews.branch_id = branches.id")
-                           .where("interviews.neighborhood_id = ? AND is_demo = ?", @neighborhood.id, 0)
-                           .order("storyteller_name")
+
     render json: @interviews
   end
-  
+
   def demo
     @interviews = Interview.select("annotations, id, image, slug, storyteller_name, summary, url")
                            .where("is_demo = ?", 1)
@@ -38,7 +48,7 @@ class InterviewsController < ApplicationController
   # PUT /interviews/1.json
   def update
     @interview = Interview.find_by_slug(params[:id])
-    
+
     # only allow public to update annotations
     annotations = params[:interview][:annotations]
 
